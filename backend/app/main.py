07650c45c1b1fb21a2f9b4259e013e777d502adb
BornfreeYan <ybf3273225997@qq.com>
2026-09-23 18:34:08 +0800

    Helloworld and health check

---
+++ backend/app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/health")# health infra
+async def health():
+    return {"status":"ok"}
+# It's Helloworld, using the command: uvicorn app.main:app --reload. Then see http://127.0.0.1:8000/docs
